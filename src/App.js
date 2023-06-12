56025f615c5b0dc1a2167605f99f72cb266e493e
Mac_Developer <araozangel842@gmail.com>
2023-06-12 22:01:57 +0300

    Starting layout structure

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ 
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+
+      <h2>Uploading images</h2>
+      <img src={logo} className="App-logo" alt="logo" style={{ width: '130px', height: '130px' }} />
+      </div>
+      <input type="file" name="" id="" />
+      <div class="images">
+        <h1>All images downloaded from db</h1>
+      </div>
     </div>
   );
 }
