8dd6227bada6f995fa7ed8f25b5f588ad7c817bc
Mac_Developer <araozangel842@gmail.com>
2023-06-15 23:07:11 +0300

    Changes:
    1- Upload pic to Firebase and return the url.
    2- Add the url and name to the Amplify db.
    3- Add styles.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,74 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import { DataStore, Amplify } from 'aws-amplify';
 import { Images } from '../src/models';
 import aws from './aws-exports';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import appFirebase from './credential';
 import './App.css';
 
 function App() {
-  Amplify.configure(aws)
+  Amplify.configure(aws);
   const [images, setImages] = useState([]);
+  const [name, setName] = useState('');
+  const [file, setFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const storage = getStorage(appFirebase);
+  const fileInputRef = useRef();
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    if (!file) return;
+    setLoading(true);
+    const refFile = ref(storage, `images/${file.name}`);
+    await uploadBytes(refFile, file);
+    const imgUrl = await getDownloadURL(refFile);
+    if (imgUrl) {
+      handleSubmit(imgUrl);
+    }
+    setFile(null);
+    setName('');
+    setTimeout(() => {
+      reload();
+    }, 2000);
+    setLoading(false);
+  };
 
   useEffect(() => {
     DataStore.query(Images).then(setImages);
   }, []);
 
-
   const ImageView = ({ image }) => (
     <div className="image">
       <h3>{image.name}</h3>
-      <img src={image.url} alt="" style={{
-        width: '150px',
-        height: '150px',
-        objectFit: 'cover',
-
-      }}/>
+      <img
+        src={image.url}
+        alt={image.name}
+        style={{
+          width: '140px',
+          height: '140px',
+          objectFit: 'contain',
+        }}
+      />
     </div>
   );
+  const handleSubmit = (url) => {
+    DataStore.save(
+      new Images({
+        name,
+        url,
+      })
+    );
+    setName('');
+    setFile('');
+    fileInputRef.current.value = null;
+  };
 
+  const reload = () => DataStore.query(Images).then(setImages);
 
   return (
     <div className="App">
@@ -36,7 +79,7 @@ function App() {
           alignItems: 'center',
         }}
       >
-        <h2>Uploading images</h2>
+        <h2>Upload your images</h2>
         <img
           src={logo}
           className="App-logo"
@@ -44,12 +87,30 @@ function App() {
           style={{ width: '130px', height: '130px' }}
         />
       </div>
-      <input type="file" name="" id="" />
-        <h1>All images downloaded from db</h1>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Enter name"
+          value={name}
+          className="input"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="file"
+          name="file"
+          id=""
+          ref={fileInputRef}
+          className="input"
+          onChange={handleFileChange}
+        />
+        <button className="btn" onClick={handleUpload}>
+          {loading ? 'Uploading...' : 'Submit'}
+        </button>
+      </div>
       <div className="images">
-        {!images.length ? 'Nothing to show' : images.map((image) => (
-          <ImageView key={image.id} image={image} />
-        )) }
+        {!images.length
+          ? 'Nothing to show'
+          : images.map((image) => <ImageView key={image.id} image={image} />)}
       </div>
     </div>
   );
